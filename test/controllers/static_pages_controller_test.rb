05b9bbced08d11364a2e87d69b0265d103390067
Sagar <070bct535@ioe.edu.np>
2019-01-10 17:58:40 +0545

    integration test complete

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,28 +12,28 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_url
 
     assert_select "title", "Home | #{@base_title}"
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get helf_path
 
     assert_select "title", "Help | #{@base_title}"
     assert_response :success
   end
 
   test "should get about" do
-  	get static_pages_about_url
+  	get about_path
 
   	assert_select "title", "About | #{@base_title}"
   	assert_response :success
   end
 
   test "shoud get contact" do
-  	get static_pages_contact_url
+  	get contact_path
 
   	assert_select "title", "Contact | #{@base_title}"
   	assert_response :success	
