1ce68e96acc829795cb716dda74aac5624b19209
Sagar <070bct535@ioe.edu.np>
2019-01-14 09:23:10 +0545

    login feature initiated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
@@ -8,6 +10,10 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help', as: 'helf'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
