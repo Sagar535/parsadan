2afca01b1bfaeab9e444633226f559f420caf453
Sagar <070bct535@ioe.edu.np>
2019-01-14 11:27:01 +0545

    - authenticate user - flash message for unsucessful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
+  	@title = "Log in"
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email])
+
+  	if user && user.authenticate(password: params[:session][:password])
+  		render html: 'success login'
+  	else
+  		flash[:danger] = "Invalid email/password combo"
+  		render 'new'
+  	end
   end
 end
