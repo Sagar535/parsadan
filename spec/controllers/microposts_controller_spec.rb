f5a383dd5fd8c9e9fd4c0a137272419359cc66fc
Sagar <070bct535@ioe.edu.np>
2019-01-30 15:35:07 +0545

    - complete rspec test for the micropost create - different context for the micropost delete defined

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe MicropostsController, type: :controller do
-	
+	# normal user
+	let(:user) {
+		create(:user)
+	}
+
+	# sample micropost 
+	let(:sample_post) {
+		{
+		params: {
+			micropost: {
+				content: 'Lorem ipsum dolor it'
+				}
+			}
+		}
+	}
+
+	describe 'POST #create' do 
+		context "non logged in user posts" do
+			it "should not create the post" do
+				expect{
+					post :create, sample_post
+				}.not_to change(Micropost, :count)
+			end
+		end
+
+		context "logged in user posts" do
+			it "should create the post" do
+				session[:user_id] = user.id
+				expect {
+					post :create, sample_post
+				}.to change(Micropost, :count).by(1)
+			end
+
+			it "should redirect to root url" do 
+				session[:user_id] = user.id
+				post :create, sample_post
+				expect(response).to redirect_to(root_url)
+			end
+		end
+	end
+
+	describe 'DELETE #destroy' do 
+		context "non logged in user" do end
+		context "non admin logged in user" do end
+		context "logged in admin" do end
+	end
 end
