a2962d556f0a83b683ec2d8e48174189222a883d
Sagar <070bct535@ioe.edu.np>
2019-01-30 13:00:25 +0545

    all context well defined

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe SessionsController, type: :controller do
 			expect(response).to be_successful
 		end
 	end
+
+	describe "POST #create" do 
+		context "when attributes are wrong" do end
+		context "when user is not activated" do end
+		context "when all is well" do end
+	end
+
+	describe "DELETE #destroy" do 
+		context "when user is not logged in" do end
+		context "when user is logged in" do end
+	end
 end
