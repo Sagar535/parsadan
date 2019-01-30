5d1efba961844b1c49c8d10afffb9d6ddd47e77f
Sagar <070bct535@ioe.edu.np>
2019-01-30 13:09:49 +0545

    -variables neccessary defined -check for when invalid params for login

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,6 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do 
+	# invalid params
+	let(:invalid_params) {
+		{
+			session: {
+				email: 'random@random.com',
+				password: 'password'
+			}
+		}
+	}
+
+	let(:valid_params) {
+		{
+			session: {
+				emial: 'random@random.com',
+				password: 'gaggag'
+			}
+		}
+	}
+
+	let (:user) {
+		create(:user)
+	}
+
+	let (:super_user) {
+		create(:super_user)
+	}
+
+	let (:not_active_user) {
+		create(:non_active_user)
+	}
+
 	describe "GET #new" do 
 		it "should get success response" do 
 			get :new
@@ -9,7 +40,12 @@ RSpec.describe SessionsController, type: :controller do
 	end
 
 	describe "POST #create" do 
-		context "when attributes are wrong" do end
+		context "when attributes are wrong" do
+			it "should render login page" do 
+				post :create, params: invalid_params, session: {}
+				expect(response).to render_template('sessions/new')
+			end
+		 end
 		context "when user is not activated" do end
 		context "when all is well" do end
 	end
