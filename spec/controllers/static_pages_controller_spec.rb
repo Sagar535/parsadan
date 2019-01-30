c43ea3b53ea0bcd56e1faa878f6a1cedcc725369
Sagar <070bct535@ioe.edu.np>
2019-01-30 10:53:17 +0545

    minor code refactors

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-	describe "GET static pages" do 
+	describe "GET #home" do 
 		it "should return home path with success" do 
 			get :home
 			expect(response).to be_successful
 		end
-
+	end
+	describe "GET #help" do 
 		it "should return help" do 
 			get :help
-			expect(response).to be_successful	
+			expect(response).to be_successful
 		end
-
+	end
+	describe "GET #about" do 
 		it "should return about" do 
 			get :about
 			expect(response).to be_successful
-		end 
-
+		end
+	end 
+	describe "GET #contact" do 
 		it "should return contact" do 
 			get :contact
 			expect(response).to be_successful
