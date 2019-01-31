d7f6f129bd73796d3dd7d208c02d373105a6d4ea
Sagar <070bct535@ioe.edu.np>
2019-01-31 17:43:46 +0545

    features for user login written

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:valid_params) { build(:user).attributes.merge(password: 'gaggag') }
+
+  scenario "able to see the login form" do 
+  	visit('/login')
+  	expect(page).to have_content(/email/i)
+  	expect(page).to have_content(/password/i)
+  end
+
+  scenario "able to login" do 
+  	visit('/login')
+  	fill_in "session_email", :with => user.email
+  	fill_in "session_password", :with => valid_params[:password]
+  	click_button "Log in"
+
+  	expect(page).to have_link('', href: user_path(user))
+  	expect(page).to have_link('', href: user_path(user))  
+  	
+  	expect(page).to have_content(user.name)
+  	expect(page).to have_content(other_user.name)
+  end
+end
