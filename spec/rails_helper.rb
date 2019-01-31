5763e74f0d7191c11bb74e24b837c8e6dc493404
Sagar <070bct535@ioe.edu.np>
2019-01-31 11:52:43 +0545

    - configuration to make shuolda matchers available

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,11 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.include ApplicationHelper
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
