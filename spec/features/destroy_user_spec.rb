804472111ff13305ab20c0919a11b045b005432b
Sagar <070bct535@ioe.edu.np>
2019-02-01 13:34:49 +0545

    - spec for admin and non admin visiting the users page

--- spec/features/destroy_user_spec.rb
+++ spec/features/destroy_user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.feature "DestroyUsers", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:super_user) { create(:user, :super) }
+
+  before do
+  	# visit '/login'
+  	# fill_in 'session_email', :with => current_user.email
+  	# fill_in 'session_password', :with => 'gaggag'
+  	# click_button 'Log in'
+
+  	login(current_user, 'gaggag')
+
+  	other_user
+  	visit '/users'
+  end
+  context "when admin visits" do
+	let(:current_user) { super_user }
+  	scenario "shows the delete" do
+		expect(page).to have_content('delete')
+		expect(page).to have_link('delete')
+  	end
+  	scenario "deletes the user" do 
+		click_link 'delete'
+		page.accept_alert
+		expect(page).to have_content('User deleted')
+  	end
+  end
+
+  context "when normal user visits" do
+	let(:current_user) { user }
+  	scenario "won't shows the delete" do
+		expect(page).not_to have_content('delete')
+  	end
+  end
 end
