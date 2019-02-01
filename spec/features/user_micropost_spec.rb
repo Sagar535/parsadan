70fc5ab7347eac7ee84dfb1c5a0f3dc4cec5d354
Sagar <070bct535@ioe.edu.np>
2019-02-01 18:08:01 +0545

    - picture added for large size error message - code refactored for user micropost specs

--- spec/features/user_micropost_spec.rb
+++ spec/features/user_micropost_spec.rb
@@ -3,36 +3,50 @@ require 'rails_helper'
 RSpec.feature "UserMicroposts", type: :feature do
   let(:user) { create(:user) }
   let(:micropost) { create(:micropost, user: user) }
+  let(:valid_params) { build(:micropost, user: user).attributes }
 
   before do 
   	login(user, 'gaggag')
+    visit '/'
+    fill_in 'micropost_content', with: params[:content]
+    attach_relevant_file if respond_to? :attach_relevant_file
+    click_button 'Post'
   end
 
-  context "post with invalid content" do 
-  	scenario "sees the error messages" do
-  		visit '/'
-  		fill_in 'micropost_content', with: ''
-  		click_button 'Post'
-  		expect(page).to have_content("Content can't be blank")
-  	end
-  end
-  context "post with invalid file format" do
-  	scenario "sees the error message" do 
-  		visit '/'
-  		fill_in 'micropost_content', with: 'lorem'
-  		attach_file 'micropost_picture', Rails.root + 'spec/fixtures/languages.csv'
-  		click_button 'Post'
-  		expect(page).to have_content("Picture You are not allowed to upload \"csv\"")
-  	end
+  context "post with invalid content" do
+    context "post with empty content" do
+      let(:params) { valid_params.merge("content" => '') }                                                                                                                                
+      scenario "sees the error messages" do
+        expect(page).to have_content("Content can't be blank")
+    	end
+    end
+
+    let(:params) { valid_params }
+    context "post with invalid file format" do
+      let(:attach_relevant_file) do
+        attach_file 'micropost_picture', Rails.root + 'spec/fixtures/languages.csv'
+      end      
+      scenario "sees the error message" do 
+        expect(page).to have_content("Picture You are not allowed to upload \"csv\"")
+      end
+    end
+
+    context "post with large file" do
+      let(:attach_relevant_file) do                                                                                                         
+        attach_file 'micropost_picture', Rails.root + 'spec/fixtures/invalid.png'
+      end
+      scenario "sees the error message" do
+        expect(page).to have_content("Picture can't be greater than 300 kilobytes")
+      end
+    end
   end
   
-  context "post with valid content" do
-  	scenario "sees the success message" do 
-  		visit '/'
-  		fill_in 'micropost_content', with: 'lorem'
-  		attach_file 'micropost_picture', Rails.root + 'spec/fixtures/screen.png'
-  		click_button 'Post'
-  		expect(page).to have_content("Micropost created!")
-  	end
-  end
+  # context "post with valid content" do
+  #   let(:params) { valid_params }
+  #   scenario "sees the success message" do 
+  # 		attach_file 'micropost_picture', Rails.root + 'spec/fixtures/screen.png'
+  # 		click_button 'Post'
+  # 		expect(page).to have_content("Micropost created!")
+  # 	end
+  # end
 end
